62e71f053626d66eb6d36c8c9b435354c70dd3d6
anbreaker <antunez19@gmail.com>
2020-09-05 13:05:53 +0200

    para probar con errores http desde otro archivo...

---
+++ src/hadlerError.js
@@ -0,0 +1,35 @@
+'use stric';
+
+const createError = require('http-errors');
+const app = require('./app');
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // console.log(err);
+  if (err.array) {
+    // error de validación
+    err.status = 422;
+    const errInfo = err.array({onlyFirstError: true})[0];
+    err.message = `El parámetro ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  res.status(err.status || 500);
+
+  if (req.originalUrl.startsWith('/api/')) {
+    // API request
+    res.json({error: err.message});
+    return;
+  }
+
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.render('error');
+});
